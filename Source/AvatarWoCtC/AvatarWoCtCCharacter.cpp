fc77fdb0a953fe48ba96b86361a18c5d2244d749
APTMTCOMP\Kyle <kylewolfeman5@gmail.com>
2018-04-05 15:05:25 -0400

    fixed issue with enum spawn points not being offset appropriately

--- Source/AvatarWoCtC/AvatarWoCtCCharacter.cpp
+++ Source/AvatarWoCtC/AvatarWoCtCCharacter.cpp
@@ -304,7 +304,7 @@ void AAvatarWoCtCCharacter::RequestLightAttack()
 		else
 		{
 			float y_offset_sign = FollowCamera->RelativeLocation.Y / FMath::Abs(FollowCamera->RelativeLocation.Y);
-			AttackComp->ActivateRangedAbility(RangedLightAttack, FVector(0.f, y_offset_sign * (float)EWidthPoints::DP_Outer_Pos, (float)EWidthPoints::DP_Outer_Neg));
+			AttackComp->ActivateRangedAbility(RangedLightAttack, FVector(0.f, y_offset_sign * (float)EWidthPoints::DP_Outer_Pos - (float)EWidthPoints::DP_None, (float)EWidthPoints::DP_Outer_Neg - (float)EWidthPoints::DP_None));
 		}
 	}
 	else
@@ -344,7 +344,7 @@ void AAvatarWoCtCCharacter::RequestHeavyAttack()
 		else
 		{
 			float y_offset_sign = FollowCamera->RelativeLocation.Y / FMath::Abs(FollowCamera->RelativeLocation.Y);
-			AttackComp->ActivateRangedAbility(RangedHeavyAttack, FVector(0.f, y_offset_sign * (float)EWidthPoints::DP_Outer_Pos, (float)EWidthPoints::DP_Outer_Neg));
+			AttackComp->ActivateRangedAbility(RangedHeavyAttack, FVector(0.f, y_offset_sign * (float)EWidthPoints::DP_Outer_Pos - (float)EWidthPoints::DP_None, (float)EWidthPoints::DP_Outer_Neg - (float)EWidthPoints::DP_None));
 		}
 	}
 	else
@@ -384,7 +384,7 @@ void AAvatarWoCtCCharacter::RequestStunAttack()
 		else
 		{
 			float y_offset_sign = FollowCamera->RelativeLocation.Y / FMath::Abs(FollowCamera->RelativeLocation.Y);
-			AttackComp->ActivateRangedAbility(RangedStunAttack, FVector(0.f, y_offset_sign * (float)EWidthPoints::DP_Outer_Pos, (float)EWidthPoints::DP_Outer_Neg));
+			AttackComp->ActivateRangedAbility(RangedStunAttack, FVector(0.f, y_offset_sign * (float)EWidthPoints::DP_Outer_Pos - (float)EWidthPoints::DP_None, (float)EWidthPoints::DP_Outer_Neg - (float)EWidthPoints::DP_None));
 		}
 	}
 	else
